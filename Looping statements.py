69a0149bae52867c920374776d908138e829e4bf
akshayathangallapally <ta7440360@gmail.com>
2025-02-18 23:40:25 +0530

    Add files via upload

---
+++ Looping statements.py
@@ -0,0 +1,20 @@
+#for loop
+a=[1,2,3,4,5]
+for i in a:
+    print(i)
+
+#using string
+b="python"      
+for i in b:
+    print(i)
+
+# while loop
+k=10
+while k<20:
+    print("hello guys")
+    k+=1
+
+#nested for
+for i in range(0,10):
+    for j in range(0,10):
+        print(i*j)
